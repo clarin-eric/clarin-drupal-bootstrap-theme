de08646c50296578162aa5d77bdc282b679e61e4
andmor <amoreira.mail@gmail.com>
2021-04-28 20:13:16 +0200

    Revert "Try with npm install"
    This reverts commit 48add7a332338254310890c56368c30fc8ed9dd2.

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ ${RM} -fr -- "${OUTPUT_DIRECTORY}" "node-modules"
 
 # Install build environment
 echo 'Installing npm dependencies ...'
-npm install
+npm ci
 
 GULP="./node_modules/gulp/bin/gulp.js"
 echo 'Using local gulp: ' ${GULP}
