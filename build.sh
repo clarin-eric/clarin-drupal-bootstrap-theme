047fd107b057aa5ddd7b0567a21573e2b7c0c264
andmor <amoreira.mail@gmail.com>
2021-04-29 14:34:31 +0200

    Added fast distribution build option for development
    Now default when calling build.sh

--- build.sh
+++ build.sh
@@ -15,13 +15,28 @@ BUILD_PACKAGE="${OUTPUT_DIRECTORY}/clarin_bootstrap-${PACKAGE_VERSION}.tar.gz"
 #gcp and grm can be installed on MacOS via brew. Run "brew install coreutils" to do so.
 RM=`which grm||which rm`  #if grm available (on Mac), use it instead of BSD rm
 
-# Cleanup potential previous build output
-${RM} -fr -- "${OUTPUT_DIRECTORY}" "node-modules"
+case "${1}" in
+    "ci" | "clean")
+        # Cleanup everything from potential previous build
+        ${RM} -fr -- "${OUTPUT_DIRECTORY}" "node-modules"
+        echo 'Installing npm dependencies ...'
+        # Install everything fresh
+        npm ci
+        ;;
+    "")
+        # Cleanup build output only
+        ${RM} -fr -- "${OUTPUT_DIRECTORY}"
+        echo 'Installing npm dependencies ...'
+        # Update pre-installed packes
+        npm install
+        ;;
+    *) 
+        echo "Invalide build parameter: \"${1}\" ... exiting ...\n"
+	    exit 1
+	    ;;
+esac
 
-# Install build environment
-echo 'Installing npm dependencies ...'
-npm ci
-
+    
 GULP="./node_modules/gulp/bin/gulp.js"
 echo 'Using local gulp: ' ${GULP}
 
@@ -31,8 +46,6 @@ mkdir "${OUTPUT_DIRECTORY}"
 curl --fail --location --show-error --silent --tlsv1 \
     "https://ftp.drupal.org/files/projects/bootstrap_barrio-${BOOTSTRAP_BARRIO_VERSION}.tar.gz" | \
 tar -x -z -p -C "${OUTPUT_DIRECTORY}"
-
-#echo 'Customising...'
 
 echo 'Building ...'
 ${GULP} "dist"
