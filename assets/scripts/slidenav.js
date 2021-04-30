cbea170c5bdbac0a82abf48915ed22403ec00ad5
andmor <amoreira.mail@gmail.com>
2021-04-30 16:38:52 +0200

    Naming: event handler "attached" rather then "bond"

--- assets/scripts/slidenav.js
+++ assets/scripts/slidenav.js
@@ -6,13 +6,12 @@
   // - Add click handler to dismiss (slide out) the mobile floating menu when clicking outside of it
   Drupal.behaviors.clarin_theme_slidenav = {
     attach: function(context, settings) {
-
-      function setDismissHandler(body, isBond) {
+      function setDismissHandler(body, isAttached) {
         var collapsingNavbar = $("div#CollapsingNavbar", body);
         //console.log(collapsingNavbar.css("position"));
         if(collapsingNavbar.css("position") === "fixed") {
-          // When the viewport is narrow enough #collapsingNavbar becomes "fixed" -> bind handler to body
-          if (!isBond) {
+          // When the viewport is narrow enough #collapsingNavbar becomes "fixed" -> attach handler to body
+          if (!isAttached) {
             body.on("click",function(event) {
               if (!event.target.classList.contains("navbar-toggler") && //avoid event recursion since the toggler is part of body
                   !collapsingNavbar[0].contains(event.target) &&  // do not close menu if clicking on it
@@ -21,14 +20,14 @@
                 $("header button.navbar-toggler", context).trigger("click");
               }
             });
-            //console.log("handler bond");
+            //console.log("handler attached");
           }
           return true;
         } else {
-          // When the viewport is wide enough  -> unbind handler from body
-          if (isBond) {
+          // When the viewport is wide enough  -> detach handler from body
+          if (isAttached) {
             body.off("click");
-            //console.log("handler unbond");
+            //console.log("handler dettached");
           }
           return false;
         }
@@ -36,10 +35,10 @@
 
       if (context === document) {
         var body = $("body", context);
-        var isBond = setDismissHandler(body);
+        var isAttached = setDismissHandler(body);
         // On resize event set click handler to dismiss the mobile floating menu when clicking outside of it
         $(window, context).on("resize", function() {
-          isBond = setDismissHandler(body, isBond);
+          isAttached = setDismissHandler(body, isAttached);
         });
       }
     }
