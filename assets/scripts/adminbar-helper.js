3b467769425d81e6657f37645f44cfe145c5ede9
andmor <amoreira.mail@gmail.com>
2021-05-12 17:17:53 +0200

    New sticky mobile menu and top-left region

--- assets/scripts/adminbar-helper.js
+++ assets/scripts/adminbar-helper.js
@@ -6,10 +6,10 @@
   // - Reposition body and floating menu when user has admin toolbar enabled.
   Drupal.behaviors.clarin_theme_adminbar_helper = {
     attach: function(context, settings) {
-      // Reposition content and floating menu
-      function repositionContent(mainToolbar, collapsingNavbar, body, toolbarElem) {
+      let prevEffectiveStickyTop = 0;
+
+      function repositionContent(mainToolbar, header, body, toolbarElem, timer) {
         let toolbarTotalHeight = toolbarElem.clientHeight;
-        const toolbarElemHeight = Math.floor(toolbarTotalHeight);
 
         // We will try to avoid calls to mainToolbar.outerHeight(true) as toolbarElem might already contain
         // all the necessary height information to reposition the content
@@ -23,7 +23,7 @@
           }
         } else {
           // When we receive a toolbar tray height -> add the main toolbar height to it
-          if (toolbarElemHeight === 0) {
+          if (toolbarElem.clientHeight === 0) {
             // Tray became invisible:
             if ($("div.toolbar-tray-horizontal.is-active", body).length) {
               // Other tray is now visible (tray switch) -> ignore as the other 
@@ -34,20 +34,53 @@
             toolbarTotalHeight = Math.floor(mainToolbar.outerHeight(true));
           } else if (toolbarElem.classList.contains("toolbar-tray-horizontal")) {
             // Toolbar tray is visible and horizontal -> [total height] = [tray top offset] + [tray height]
-            toolbarTotalHeight = Math.floor(toolbarElem.offsetTop) + toolbarElemHeight;
+            toolbarTotalHeight = Math.floor(toolbarElem.offsetTop + toolbarElem.clientHeight);
           } else {
             // Toolbar tray is visible but vertical -> [total height] = [main toolbar height]
             toolbarTotalHeight = Math.floor(mainToolbar.outerHeight(true));
           }
         }
-        
-        // Reposition content
-        if (collapsingNavbar.css("position") === "fixed") {
-          collapsingNavbar.css("padding-top", toolbarTotalHeight + 15 + "px");
-        } else {
-          collapsingNavbar.css("padding-top", "");
-        }
-        body.css("padding-top", toolbarTotalHeight + "px");
+
+        //  When the toolbar position is "fixed", the sticky menu "top" also needs to be repositioned.
+        // We do it in the next frame (via requestAnimationFrame()) since when this handler runs, the
+        // admin toolbar has not yet updated the classes of the body
+
+        // Check difference between default "top" in the previous state (prevEffectiveStickyTop) and current observed "top" 
+        //  - > apply the same offset
+        const headerHeight = Math.floor(header.outerHeight(true));
+        const headerFixedStickyTop = toolbarTotalHeight - headerHeight; // the `top` property of the sticky header when the admin toolbar is fixed
+        const headerObservedStickyTop = Math.floor(parseFloat(header.css("top")));
+        let topOffset = Math.abs(headerObservedStickyTop - prevEffectiveStickyTop);
+
+        window.cancelAnimationFrame(timer);
+        timer = window.requestAnimationFrame(() => {
+
+          // The original core/modules/toolbar/js/views/ToolbarVisualView.js tries to position the body
+          //   taking into account the admin toolbar height. This does not work for certain window widths
+          //   when the flex container of the same bar (or tray) expands to two lines. 
+          // - > Reset the padding-top set by core/modules/toolbar/js/views/ToolbarVisualView.js and
+          //   reposition content ourselves
+
+          // Reposition body
+          body.css("padding-top", toolbarTotalHeight + "px");
+
+          if (header.css("position") === "sticky") {
+            if (body.hasClass("toolbar-fixed")) {
+              // Tablet screen widths (480px < width < 1200px) the admin toolbar becomes `position: fixed`
+              // shift our menu header down by the toolbar height
+              header.css("top", headerFixedStickyTop + topOffset + "px");
+              prevEffectiveStickyTop = headerFixedStickyTop;
+            } else {
+              // Phone screen widths (width < 480px) the admin toolbar becomes `position: absolute`
+              header.css("top", topOffset - headerHeight + "px");
+              prevEffectiveStickyTop = 0 - headerHeight;
+            }
+          } else {
+            // Reset when menu is not in use anymore
+            header.css("top", "");
+            prevEffectiveStickyTop = 0;
+          }
+        });
       }
 
       if (context === document) {
@@ -55,15 +88,15 @@
           // Admin toolbar is active:
           const body = $("body", context);
           const mainToolbar = $("#toolbar-bar", body);
-          const collapsingNavbar = $("#CollapsingNavbar", body);
+          const header = $("#header", body);
 
           // Use a resize observer on the admin toolbar and each of its trays to detect changes in height
           // Reposition the content when they happen
-          $("#toolbar-bar, .toolbar-tray", body).each((index, adminBarElement) => {         
+          $("#toolbar-bar, .toolbar-tray", body).each((index, adminBarElement) => {
+            let timer = null;
             const trayResizeObserver = new ResizeObserver(entries => {
               for (let entry of entries) {
-                //console.log("Admin toolbar: #" + entry.target.id + " height changed to: " + entry.target.clientHeight);
-                repositionContent(mainToolbar, collapsingNavbar, body, entry.target);
+                repositionContent(mainToolbar, header, body, entry.target, timer);
               }
             });
             trayResizeObserver.observe(adminBarElement);
--- assets/scripts/slidenav.js
+++ assets/scripts/slidenav.js
@@ -2,42 +2,102 @@
 
   "use strict";
 
-  // CLARIN slidenav helper methods for the top navbar menu
-  // - Add click handler to dismiss (slide out) the mobile floating menu when clicking outside of it
+  // CLARIN slidenav helper methods for the top navbar menu. The slidenav is only used on screens narrower
+  // than 1200px and when activated in the theme options.
+  // - When the window width is narrower than 1200px, add a click handler to dismiss (slide out) the mobile floating
+  //     menu when clicking outside of it.
+  // - Remove the handler if the window becomes wider.
   Drupal.behaviors.clarin_theme_slidenav = {
     attach: function(context, settings) {
-      function setDismissHandler(body, collapsingNavbar, isAttached) {
-        //console.log(collapsingNavbar.css("position"));
-        if(collapsingNavbar.css("position") === "fixed") {
+      let handlersAttached;
+      let headerHeight;
+      let lastScrollPosition;
+      let body;
+      let header;
+      let collapsingNavbar;
+      let adminBar;
+
+      function handleClickOut (event) {
+        if (!header.contains(event.target) &&  // do not close menu if clicking on it
+            (adminBar == null ? true : !adminBar.contains(event.target)) &&  // do not close menu if clicking on adminBar
+            collapsingNavbar.css("display") !== "none") {  // is closed
+            
+          //collapse navbar
+          collapsingNavbar.collapse("hide");
+        }
+      }
+
+      function handleScroll () {
+        headerHeight = parseFloat(window.getComputedStyle(header).height);
+        let pageScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        let adminBarHeight = parseFloat(body.style.paddingTop) || 0;
+        let fixedAdminBarHeight = body.style.paddingTop && body.classList.contains("toolbar-fixed") ? adminBarHeight : 0;
+        let defaultStickyTop = fixedAdminBarHeight - headerHeight;
+        let observedStickyTop = header.style.top ? parseFloat(header.style.top) : 0;
+        let relativeDelta = lastScrollPosition - pageScrollPosition + observedStickyTop;
+
+        if (relativeDelta > defaultStickyTop && relativeDelta < fixedAdminBarHeight) {
+          //in betwen stage
+          header.style.top = relativeDelta + "px";
+
+          if (pageScrollPosition > (fixedAdminBarHeight === 0 ?
+            headerHeight + adminBarHeight : headerHeight)) {
+            header.classList.add("shadow-header");          }
+        } else if (relativeDelta < defaultStickyTop) {
+          // fully disappears
+          header.style.top =  defaultStickyTop + "px";
+          header.classList.remove("shadow-header");
+        } else if (relativeDelta > fixedAdminBarHeight) {
+          // fully appears
+          header.style.top =  fixedAdminBarHeight + "px";
+
+          if (pageScrollPosition <= (fixedAdminBarHeight === 0 ?
+            adminBarHeight : 0)) {
+            header.classList.remove("shadow-header");
+          }
+        }
+        lastScrollPosition = pageScrollPosition;
+      }
+
+
+      function setHandlers(handlersAttached) {
+        body = document.body;
+        adminBar = document.getElementById("toolbar-administration");
+        header = document.getElementById("header");
+        collapsingNavbar = $("#CollapsingNavbar", body);
+        lastScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        window.setTimeout(() => {
+          headerHeight = parseFloat(window.getComputedStyle(header).height); ///Track page scroll with extra XYpx "delay"
+        });
+
+       
+        if(collapsingNavbar.css("position") === "absolute") {
           // When the viewport is narrow enough #collapsingNavbar becomes "fixed" -> attach handler to body
-          if (!isAttached) {
-            body.on("click",function(event) {
-              if (!collapsingNavbar[0].contains(event.target) &&  // do not close menu if clicking on it
-                  collapsingNavbar.css("display") !== "none") {  // is closed
-                //collapse navbar
-                collapsingNavbar.collapse("hide");
-              }
-            });
-            //console.log("handler attached");
+          if (!handlersAttached) {
+            // Add listeners
+            body.addEventListener("click", handleClickOut, true);
+            window.addEventListener("scroll", handleScroll, true);
+            //console.log("handlers attached");
           }
           return true;
         } else {
           // When the viewport is wide enough  -> detach handler from body
-          if (isAttached) {
-            body.off("click");
-            //console.log("handler dettached");
+          if (handlersAttached) {
+            // Remove listeners
+            body.removeEventListener("click", handleClickOut, true);
+            window.removeEventListener("scroll", handleScroll, true);
+            //console.log("handlers dettached");
           }
           return false;
         }
       }
 
       if (context === document) {
-        var body = $("body", context);
-        var collapsingNavbar = $("#CollapsingNavbar", body);
-        var isAttached = setDismissHandler(body, collapsingNavbar);
-        // On resize event set click handler to dismiss the mobile floating menu when clicking outside of it
+        handlersAttached = setHandlers();
+
+        // On resize event set click handler to dismiss the mobile floating menu
         $(window, context).on("resize", function() {
-          isAttached = setDismissHandler(body, collapsingNavbar, isAttached);
+          handlersAttached = setHandlers(handlersAttached);
         });
       }
     }
