8ceb9b6a89a34508a85c1a9885b1cb7389587e67
andmor <amoreira.mail@gmail.com>
2021-05-17 19:35:58 +0200

    Allow multiple header_large paragraphs

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -14,8 +14,9 @@
       }
       // landingpage header image to background
       $( ".field--name-field-content-blocks" ).each(function() {
-        var image = $(this).find(".paragraph--type--header-large img, .paragraph--type--cta img").attr("src");
-        $(this).find(".paragraph--type--header-large, .paragraph--type--cta").css("background-image", "url('"+image+"')");
+        $(this).find(".paragraph--type--header-large, .paragraph--type--cta").each(function() {
+          $(this).css("background-image", "url('" + $(this).find("img").attr("src") + "')");
+        });
       });
 
       // Form field label as placeholder
