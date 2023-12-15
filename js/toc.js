0316a13253d09c887f7b2e334fdb032f04a91b5d
André Moreira <amoreira.mail@gmail.com>
2023-12-15 20:46:43 +0100

    Use requestAnimationFrame instead of setTimeout

--- js/toc.js
+++ js/toc.js
@@ -8,9 +8,11 @@
     attach: function attach(context) {
       if (context === document && document.readyState !== "loading") {
         $(function initToc() {
+          let timer = null;
           const navSelector = "#toc";
           const scopeSelector = "#tocscope";
-          window.setTimeout(() => {
+          window.cancelAnimationFrame(timer);
+          timer = window.requestAnimationFrame(() => {
             Toc.init({
               $nav: $(navSelector, context),
               $scope: $(scopeSelector, context)
