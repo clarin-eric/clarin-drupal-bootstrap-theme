4ee840294dd26b6a13ea0a11b2acf2044966b2dd
André Moreira <amoreira.mail@gmail.com>
2023-12-15 20:33:02 +0100

    Use setTimeout

--- js/toc.js
+++ js/toc.js
@@ -10,15 +10,17 @@
         $(function initToc() {
           const navSelector = "#toc";
           const scopeSelector = "#tocscope";
-          Toc.init({
-            $nav: $(navSelector, context),
-            $scope: $(scopeSelector, context)
+          window.setTimeout(() => {
+            Toc.init({
+              $nav: $(navSelector, context),
+              $scope: $(scopeSelector, context)
+            });
+            const body = $("body", context);
+            const scrollSpy = new bootstrap.ScrollSpy(body, {
+              target: navSelector
+            });
+            scrollSpy.refresh();
           });
-          const body = $("body", context);
-          const scrollSpy = new bootstrap.ScrollSpy(body, {
-            target: navSelector
-          });
-          scrollSpy.refresh();
         });
       }
     }
